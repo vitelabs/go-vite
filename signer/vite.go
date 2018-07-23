eb2dd8cb3a2c457cc3c2e76ddd8eccc9e1df3c3c
tiantao <tiantao@vite.org>
2018-07-23 20:21:37 +0800

    fix cycle import

---
+++ signer/vite.go
@@ -0,0 +1,11 @@
+package signer
+
+import (
+	"github.com/vitelabs/go-vite/wallet"
+	"github.com/vitelabs/go-vite/ledger/handler_interface"
+)
+
+type Vite interface {
+	WalletManager() *wallet.Manager
+	Ledger() handler_interface.Manager
+}
