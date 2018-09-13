32d9892fc263fa93ce4d3bbb578c7ef6bcfb0ddb
vite-crzn <gexin@vite.org>
2018-09-13 15:42:11 +0800

    signer in generator

---
+++ generator/interface.go
@@ -0,0 +1,10 @@
+package generator
+
+import (
+	"github.com/vitelabs/go-vite/common/types"
+)
+
+type SignManager interface {
+	SignData(a types.Address, data []byte) (signedData, pubkey []byte, err error)
+	SignDataWithPassphrase(a types.Address, passphrase string, data []byte) (signedData, pubkey []byte, err error)
+}
