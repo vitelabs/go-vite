5fc775d8dbda38046a512092a47e98b652702d86
tiantao <tiantao@vite.org>
2018-09-20 11:04:26 +0800

    pow :  nonce + address + prehash < target

--- pow/pow.go
+++ pow/pow.go
@@ -6,13 +6,14 @@ import (
 )
 
 // IN MY 2017 MACBOOK PRO which cpu is---- Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz----, that target costs about 2 seconds
-const DUMMY_TARGET = "000003FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+var DummyTarget, _ = new(big.Int).SetString("000003FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)
 
-// data = prehash + address; nonce + data < target
+// data = Hash(address + prehash); nonce + data < target. if prehash == nil {data = Hash(address)}
 func GetPowNonce(target *big.Int, data []byte) *big.Int {
 	if target == nil {
-		target, _ = new(big.Int).SetString(DUMMY_TARGET, 16)
+		return nil
 	}
+
 	csprng := crypto.GetEntropyCSPRNG(32)
 	from := new(big.Int).SetBytes(csprng)
 	calc := new(big.Int)
@@ -25,4 +26,11 @@ func GetPowNonce(target *big.Int, data []byte) *big.Int {
 		from = from.Add(from, step)
 	}
 	return from
+}
+
+func CheckNonce(target, nonce *big.Int, data []byte) bool {
+	if target == nil || nonce == nil {
+		return false
+	}
+	return new(big.Int).SetBytes(crypto.Hash256(nonce.Bytes(), data)).Cmp(target) < 0
 }
--- pow/pow_test.go
+++ pow/pow_test.go
@@ -2,32 +2,54 @@ package pow_test
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/vitelabs/go-vite/crypto"
 	"github.com/vitelabs/go-vite/pow"
+	"math"
+	"math/big"
 	"testing"
 	"time"
 )
 
-func TestPow(t *testing.T) {
-	N := 1000
+func TestGetPowNonce(t *testing.T) {
+	N := 10000
 	data := crypto.Hash256([]byte{1})
-	timesum := int64(0)
-	timeMax := int64(0)
-	timeMin := int64(1 << 31)
+	timeList := make([]int64, N)
 	for i := 0; i < N; i++ {
 		startTime := time.Now()
-		nonce := pow.GetPowNonce(nil, data)
-
+		nonce := pow.GetPowNonce(pow.DummyTarget, data)
+		assert.True(t, pow.CheckNonce(pow.DummyTarget, nonce, data))
 		d := time.Now().Sub(startTime).Nanoseconds()
 		fmt.Println("#", i, ":", d/1e6, "ms", "nonce", nonce)
-		timesum += d
-		if d > timeMax {
-			timeMax = d
-		}
-		if d < timeMin {
-			timeMin = d
-		}
+		timeList[i] = d
 	}
 
-	println("average", timesum/(int64(N))/1e6, "ms", "max", timeMax, "ns", "min", timeMin, "ns")
+	d, _ := new(big.Int).SetString("1", 10)
+	assert.False(t, pow.CheckNonce(pow.DummyTarget, d, data))
+	max, min, timeSum, average, std := statistics(timeList)
+	fmt.Println("average", average, "max", max, "min", min, "sum", timeSum, "standard deviation", std)
+}
+
+func statistics(data []int64) (timeMax, timeMin, timeSum int64, average, std float64) {
+	timeSum = 0
+	timeMax = 0
+	timeMin = 1 << 31
+	average = 0
+	std = 0
+	for _, v := range data {
+		timeSum += v
+		if v > timeMax {
+			timeMax = v
+		}
+		if v < timeMin {
+			timeMin = v
+		}
+	}
+	average = float64(timeSum) / float64(len(data))
+	vSum := float64(0)
+	for _, v := range data {
+		vSum += (float64(v) - average) * (float64(v) - average)
+	}
+	std = math.Sqrt(vSum / float64(len(data)))
+	return timeMax, timeMin, timeSum, average, std
 }
