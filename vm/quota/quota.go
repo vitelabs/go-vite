3257b44b61c8e16e30bd039e2b8e7fb372ddf268
viteLiz <chenping@vite.org>
2018-10-10 20:19:06 +0800

    simple quota calc

--- vm/quota/quota.go
+++ vm/quota/quota.go
@@ -23,9 +23,15 @@ func GetPledgeQuota(db quotaDb, beneficial types.Address) uint64 {
 	quotaTotal, _ := CalcQuota(db, beneficial, false)
 	return quotaTotal
 }
-
-// TODO change pow bool to difficulty *big.Int
 func CalcQuota(db quotaDb, addr types.Address, pow bool) (quotaTotal uint64, quotaAddition uint64) {
+	if pow {
+		return CalcQuotaV2(db, addr, defaultDifficulty)
+	} else {
+		return CalcQuotaV2(db, addr, helper.Big0)
+	}
+}
+
+func CalcQuotaV1(db quotaDb, addr types.Address, pow bool) (quotaTotal uint64, quotaAddition uint64) {
 	// quotaInit = quotaLimitForAccount * (1 - 2/(1 + e**(fDifficulty * difficulty + fPledge * snapshotHeightGap * pledgeAmount)))
 	// 				- quota used by prevBlock referring to the same snapshot hash
 	// quotaAddition = quotaLimitForAccount * (1 - 2/(1 + e**(fDifficulty * difficulty + fPledge * snapshotHeightGap * pledgeAmount)))
@@ -36,7 +42,7 @@ func CalcQuota(db quotaDb, addr types.Address, pow bool) (quotaTotal uint64, quo
 	// user account gets extra quota to send or receive a transaction if calc PoW, extra quota is decided by difficulty
 	// contract account only gets quota via pledge
 	// user account genesis block(a receive block) must calculate a PoW to get quota
-	// TODO Following code is just a simple implementation for test net.
+	// Following code is just a simple implementation for test net.
 	defer monitor.LogTime("vm", "CalcQuota", time.Now())
 	quotaLimitForAccount := quotaLimit
 	quotaInitBig := new(big.Int).Div(contracts.GetPledgeBeneficialAmount(db, addr), quotaByPledge)
@@ -159,4 +165,98 @@ func UseQuotaForData(data []byte, quotaLeft uint64) (uint64, error) {
 
 func IsPoW(nonce []byte) bool {
 	return len(nonce) > 0
+}
+
+func calcQuotaInSections(pledgeAmount *big.Int, difficulty *big.Int) uint64 {
+	x := big.NewFloat(0)
+	floatTmp := new(big.Float)
+	if pledgeAmount.Sign() > 0 {
+		floatTmp.SetInt(pledgeAmount)
+		x.Mul(paramA, floatTmp)
+	}
+	if difficulty.Sign() > 0 {
+		floatTmp.SetInt(difficulty)
+		floatTmp.Mul(paramB, floatTmp)
+		x.Add(x, floatTmp)
+	}
+	return 0
+}
+
+var defaultDifficulty = new(big.Int).SetUint64(0xffffffc000000000)
+
+func CalcQuotaV2(db quotaDb, addr types.Address, difficulty *big.Int) (quotaTotal uint64, quotaAddition uint64) {
+	pledgeAmount := contracts.GetPledgeBeneficialAmount(db, addr)
+	isPoW := difficulty.Sign() > 0
+	currentSnapshotHash := db.CurrentSnapshotBlock().Hash
+	prevBlock := db.PrevAccountBlock()
+	quotaUsed := uint64(0)
+	flag := false
+	difficultyForCalc := difficulty
+	for {
+		if prevBlock != nil && currentSnapshotHash == prevBlock.SnapshotHash {
+			flag = IsPoW(prevBlock.Nonce)
+			// quick fail on a receive error block referencing to the same snapshot block
+			if prevBlock.BlockType == ledger.BlockTypeReceiveError {
+				return 0, 0
+			}
+			if IsPoW(prevBlock.Nonce) {
+				if flag {
+					// only one block gets extra quota when referencing to the same snapshot block
+					return 0, 0
+				}
+				flag = true
+				difficultyForCalc = defaultDifficulty
+			}
+			quotaUsed = quotaUsed + prevBlock.Quota
+			prevBlock = db.GetAccountBlockByHash(&prevBlock.PrevHash)
+		} else {
+			x := new(big.Float).SetPrec(precForFloat).SetUint64(0)
+			tmpFLoat := new(big.Float).SetPrec(precForFloat)
+			if prevBlock == nil {
+				tmpFLoat.SetUint64(helper.Min(maxQuotaHeightGap, db.CurrentSnapshotBlock().Height))
+			} else {
+				tmpFLoat.SetUint64(helper.Min(maxQuotaHeightGap, db.CurrentSnapshotBlock().Height-db.GetSnapshotBlockByHash(&prevBlock.SnapshotHash).Height))
+			}
+			x.Mul(tmpFLoat, paramA)
+			tmpFLoat.SetInt(pledgeAmount)
+			x.Mul(tmpFLoat, x)
+			quotaWithoutPoW := uint64(getIndexInSection(x)) * quotaForSection
+			if flag || isPoW {
+				tmpFLoat.SetInt(difficultyForCalc)
+				tmpFLoat.Mul(tmpFLoat, paramB)
+				x.Add(x, tmpFLoat)
+				quotaTotal := uint64(getIndexInSection(x)) * quotaForSection
+				return quotaTotal, quotaTotal - quotaWithoutPoW
+			}
+			return quotaWithoutPoW, 0
+		}
+	}
+}
+
+// Get the largest index
+// which makes sectionList[index] <= x
+func getIndexInSection(x *big.Float) int {
+	return getIndexInSectionRange(x, 0, len(sectionList)-1)
+}
+func getIndexInSectionRange(x *big.Float, left, right int) int {
+	if left == right {
+		return getExactIndex(x, left)
+	}
+	mid := (left + right + 1) / 2
+	cmp := sectionList[mid].Cmp(x)
+	if cmp == 0 {
+		return mid
+	} else if cmp > 0 {
+		return getIndexInSectionRange(x, left, mid-1)
+	} else {
+		return getIndexInSectionRange(x, mid, right)
+	}
+}
+
+func getExactIndex(x *big.Float, index int) int {
+	if sectionList[index].Cmp(x) <= 0 || index == 0 {
+		return index
+	} else {
+		return index - 1
+	}
 }
