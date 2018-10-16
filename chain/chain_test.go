2ba25da6b748aafb04ad04fbd1e6e414f32e56b2
lyd00 <yanda@vite.org>
2018-10-16 17:47:03 +0800

    Snapshot block computeHash not need publickey

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -13,7 +13,7 @@ func getChainInstance() Chain {
 		home := common.HomeDir()
 
 		innerChainInstance = NewChain(&config.Config{
-			DataDir: filepath.Join(home, "Library/GVite/testdata"),
+			DataDir: filepath.Join(home, "viteisbest"),
 			//Chain: &config.Chain{
 			//	KafkaProducers: []*config.KafkaProducer{{
 			//		Topic:      "test",
