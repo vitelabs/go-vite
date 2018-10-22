af8fe7ee89ff1627a626e3d66d46323a8bf65af3
vite-crzn <gexin@vite.org>
2018-10-22 19:58:21 +0800

    add new quota err

--- rpcapi/api/error_table.go
+++ rpcapi/api/error_table.go
@@ -51,6 +51,7 @@ func init() {
 	concernedErrorMap[ErrDecryptKey.Error()] = ErrDecryptKey
 	concernedErrorMap[AddressAlreadyUnLocked.Error()] = AddressAlreadyUnLocked
 	concernedErrorMap[ErrBalanceNotEnough.Error()] = ErrBalanceNotEnough
+	concernedErrorMap[ErrQuotaNotEnough.Error()] = ErrQuotaNotEnough
 }
 
 func TryMakeConcernedError(err error) (newerr error, concerned bool) {
