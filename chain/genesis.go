2b8831d01a0c255c61702fe72a7a44123fb0ef26
viteshan <viteshan@163.com>
2018-10-30 15:25:32 +0800

    add update

--- chain/genesis.go
+++ chain/genesis.go
@@ -1,6 +1,10 @@
 package chain
 
 import (
+	"math/big"
+	"strconv"
+	"time"
+
 	"github.com/vitelabs/go-vite/common/helper"
 	"github.com/vitelabs/go-vite/common/types"
 	"github.com/vitelabs/go-vite/ledger"
@@ -8,9 +12,6 @@ import (
 	"github.com/vitelabs/go-vite/vm/contracts"
 	"github.com/vitelabs/go-vite/vm_context"
 	"github.com/vitelabs/go-vite/vm_context/vmctxt_interface"
-	"math/big"
-	"strconv"
-	"time"
 )
 
 var GenesisSnapshotBlock ledger.SnapshotBlock
@@ -218,31 +219,31 @@ func genesisRegisterBlock() (ledger.AccountBlock, vmctxt_interface.VmDatabase) {
 	}
 
 	addrStrList := []string{
-		"vite_39f1ede9ab4979b8a77167bfade02a3b4df0c413ad048cb999",
-		"vite_d7e73d4c7d07746e5015f39e7e037fe4d3577585e0a60ea3b4",
-		"vite_e9b7307aaf51818993bb2675fd26a600bc7ab6d0f52bc5c2c1",
-		"vite_73d6bf04045038a724a7b91eedeecfffabf04e6d9209994dd4",
-		"vite_a60e507124c2059ccb61039870dac3b5219aca014abc3807d0",
-		"vite_93a9bd77fb0ae68d4f23219afe08f631d5a6f30f2f8bf62341",
-		"vite_aad4536fd06e37461ae1baabcf2a7bd958a77b1a739bb05822",
-		"vite_42f326d36cad0fb772552943e6805c3bf0dfd7e837cfd5dc15",
-		"vite_9cfaab0b94b3c538db63f9219cf3701173e8b5ef0b15e046a9",
-		"vite_49696c180e80acc668a567e30187797e5b18e0c7abf33014e2",
-		"vite_918715bae54a3f221c1bcb3885467326a6d10c234fb6ca93a1",
-		"vite_da36f3623e2a60817573ff6b407f67c5c16165c5a0356c7b0e",
-		"vite_5d4ddbdb4b541210fc9d7c2cb28c46714ab0c64b6c8564e60a",
-		"vite_a1c1342c0651a7743e687890f749e1f08d7e1f447ad8ebf896",
-		"vite_9acca69467d586d80fe44f7fd39c93fa5c2b5e0ac267f9deb5",
-		"vite_d4c974b6d8ec30f32add577d417e62b34dd94f122d5ba49000",
-		"vite_7f85cc5b6a4d2f52955b811aad9db3e8b201adb92942a146be",
-		"vite_3d1d81b4a578885774bbfe471d1111be1da636f18cd13cbace",
-		"vite_eec4df5cd039c2590665251e524715f4e67e0034af556bb793",
-		"vite_7eb26bd846c416db979800e88f4ba2616a1093645b1b77726a",
-		"vite_5eff9251b046b5c4d8659c80e8db85aa5a131b51894f8a35ea",
-		"vite_fc551abb96ed191b081c61c31e08ada8e02cd4d15261a5213e",
-		"vite_72857dc260843374244e859dba7cba5de8c0db919ca2a444a8",
-		"vite_f13e8ca8c1c05aeec545c35a1e573b11563fac07fd634dca16",
-		"vite_059b6ec803981b75c2e36e8e6f9acffb142c7a245cd025989e",
+		"vite_b1c00ae7dfd5b935550a6e2507da38886abad2351ae78d4d9a",
+		"vite_8431a5acb599da19529c3e8bd099087a9d550fb29039dada28",
+		"vite_891907ccc07338b84789ce66b4555b03d7e4bb67ce270ba411",
+		"vite_9bb3e24453ab22c51d4cb34e1a11149d5a515b7cab1cc50cf9",
+		"vite_18e605c19ea97381639921d240ae5222caec8dc3c1a7352033",
+		"vite_fbfced2e2678906d4ecf181b8bdfedca8fe75c29388d189a14",
+		"vite_0325da1c8782b5ff102ee2cf77d727066d16c98347e703e622",
+		"vite_5660160a39215c8bf078dcba134d912b46d58333930991ed47",
+		"vite_c840786dae8f182cc2045d6754becf3a2ca63872841ee1d624",
+		"vite_f3ebd4ca3302e7ed0ca30b704b918364af81ec626ee1f66726",
+		"vite_f2e8ebbf034850cf73a4692687eb5e1df84faad964969f040a",
+		"vite_f4d18f7844c31a73ae3e09acaf6524cf9afb320726cad591a7",
+		"vite_15acba7c5848c4948b9489255d126ddc434cf7ad0fbbe1c351",
+		"vite_13fc91ca355265fcdb52558a7170c2850c51fa4a83bdeb07f4",
+		"vite_481fa2e37a577fab051c2c31f82d7d0ea1404dda1fdf8454b7",
+		"vite_f89582443e48ca314234d0f8417032f64634f4b72331663b06",
+		"vite_86484cad2df02abc763f980f7cbf8ab95995f2dab43b9f57c2",
+		"vite_f25da49d7efcc0fa270e456b7906175b63ec55c7a104759a98",
+		"vite_43effdd453df7f53e5eca2a15345b122caf2b528f4b1e4f9b2",
+		"vite_24a2fad703b14791380afbe8c62bfd8808e2436b513b1b597f",
+		"vite_4ba2ffe98b0fabe22057dd55b24c091d7cc0448b38a88e9b62",
+		"vite_181600228e5c428565f9b429ec23315161f9b0f2b03561dfc9",
+		"vite_9fd1bfbbc0fa526809331185a0a49f161ee35156494652bb0d",
+		"vite_7c41774d944a5bf23715c25b47d581c410162d5fa872548ab1",
+		"vite_6af8f3c871aa4cd9b1c1697cfb70387bd28d445e4c77ca3a25",
 	}
 	var addrList []types.Address
 
