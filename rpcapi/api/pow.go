80405a202fe7307184b0f72c8dd27c1cc75f3603
tiantao <tiantao@vite.org>
2018-10-31 22:49:16 +0800

    waiting pow api

--- rpcapi/api/pow.go
+++ rpcapi/api/pow.go
@@ -1,16 +1,35 @@
 package api
 
 import (
+	"encoding/binary"
+	"errors"
 	"github.com/vitelabs/go-vite/common/types"
 	"github.com/vitelabs/go-vite/pow"
+	"math/big"
 )
 
 type Pow struct {
 }
 
-func (p Pow) GetPowNonce(difficulty string, data types.Hash) []byte {
+func (p Pow) GetPowNonce(difficulty string, data types.Hash) ([]byte, error) {
 	log.Info("GetPowNonce")
-	b, _ := pow.GetPowNonce(nil, data)
-	log.Info("GetPowNonce End")
-	return b[:]
+	nonceStr := ""
+	nonceBig, ok := new(big.Int).SetString(nonceStr, 16)
+	if !ok {
+		return nil, errors.New("wrong nonce str")
+	}
+	nonceUint64 := nonceBig.Uint64()
+	nn := make([]byte, 8)
+	binary.LittleEndian.PutUint64(nn[:], nonceUint64)
+
+	bd, ok := new(big.Int).SetString(difficulty, 16)
+	if !ok {
+		return nil, errors.New("wrong nonce difficulty")
+	}
+
+	if !pow.CheckPowNonce(bd, nn, data.Bytes()) {
+		return nil, errors.New("check nonce failed")
+	}
+
+	return nn, nil
 }
