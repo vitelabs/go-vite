aec1ab8ef0e27349b115c58dcde2268c31458e18
viteLiz <chenping@vite.org>
2018-11-07 19:00:29 +0800

    delete reward method

--- rpcapi/api/register.go
+++ rpcapi/api/register.go
@@ -4,10 +4,8 @@ import (
 	"github.com/vitelabs/go-vite/chain"
 	"github.com/vitelabs/go-vite/common/types"
 	"github.com/vitelabs/go-vite/consensus"
-	"github.com/vitelabs/go-vite/consensus/core"
 	"github.com/vitelabs/go-vite/log15"
 	"github.com/vitelabs/go-vite/vite"
-	"github.com/vitelabs/go-vite/vm/contracts"
 	"github.com/vitelabs/go-vite/vm/contracts/abi"
 	"github.com/vitelabs/go-vite/vm_context"
 )
@@ -51,13 +49,6 @@ type RegistrationInfo struct {
 	WithdrawHeight string        `json:"withdrawHeight"`
 	WithdrawTime   int64         `json:"withdrawTime"`
 	CancelHeight   string        `json:"cancelHeight"`
-	MinRewardTime  int64         `json:"minRewardTime"`
-}
-
-type RewardInfo struct {
-	Reward    string `json:"reward"`
-	StartTime int64  `json:"startTime"`
-	EndTime   int64  `json:"endTime"`
 }
 
 func (r *RegisterApi) GetRegistrationList(gid types.Gid, pledgeAddr types.Address) ([]*RegistrationInfo, error) {
@@ -69,15 +60,6 @@ func (r *RegisterApi) GetRegistrationList(gid types.Gid, pledgeAddr types.Addres
 	list := abi.GetRegistrationList(vmContext, gid, pledgeAddr)
 	targetList := make([]*RegistrationInfo, len(list))
 	if len(list) > 0 {
-		genesisTime := vmContext.GetGenesisSnapshotBlock().Timestamp
-		groupInfo := abi.GetConsensusGroup(vmContext, gid)
-		reader := core.NewReader(*genesisTime, groupInfo)
-		periodTime, err := reader.PeriodTime()
-		if err != nil {
-			return nil, err
-		}
-		genesisTimeInt := genesisTime.Unix()
-
 		for i, info := range list {
 			targetList[i] = &RegistrationInfo{
 				Name:           info.Name,
@@ -87,29 +69,10 @@ func (r *RegisterApi) GetRegistrationList(gid types.Gid, pledgeAddr types.Addres
 				WithdrawHeight: uint64ToString(info.WithdrawHeight),
 				WithdrawTime:   getWithdrawTime(snapshotBlock.Timestamp, snapshotBlock.Height, info.WithdrawHeight),
 				CancelHeight:   uint64ToString(info.CancelHeight),
-				MinRewardTime:  contracts.CalcMinRewardTime(info, genesisTimeInt, periodTime),
 			}
 		}
 	}
 	return targetList, nil
-}
-
-func (r *RegisterApi) GetReward(gid types.Gid, name string) (*RewardInfo, error) {
-	snapshotBlock := r.chain.GetLatestSnapshotBlock()
-	vmContext, err := vm_context.NewVmContext(r.chain, &snapshotBlock.Hash, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-	info := abi.GetRegistration(vmContext, gid, name)
-	if info == nil {
-		return nil, nil
-	}
-	startIndex, endIndex, reward, periodTime, err := contracts.CalcReward(vmContext, info, gid)
-	if err != nil {
-		return nil, err
-	}
-	genesisTime := vmContext.GetGenesisSnapshotBlock().Timestamp.Unix()
-	return &RewardInfo{*bigIntToString(reward), contracts.IndexToTime(startIndex, genesisTime, periodTime), contracts.IndexToTime(endIndex+1, genesisTime, periodTime)}, nil
 }
 
 type CandidateInfo struct {
