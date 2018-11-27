e06564107dd8f9510c217c666fc1ebe7b5e2c3ee
lyd00 <yanda@vite.org>
2018-11-27 21:05:47 +0800

    Add benchmark that query get snapshot blocks by hash

---
+++ chain/benchmark/querySnapshotBlock_test.go
@@ -0,0 +1,13 @@
+package chain_benchmark
+
+import "testing"
+
+func Benchmark_GetSnapshotBlocksByHash(b *testing.B) {
+	//chainInstance := newChainInstance("insertAccountBlock", false)
+	//
+	//const (
+	//	QUERY_NUM_LIMIT = 100 * 10000
+	//)
+	//chainInstance.GetSnapshotBlocksByHash()
+
+}
