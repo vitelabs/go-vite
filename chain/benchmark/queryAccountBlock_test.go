cdf1fafea6b6b08f76674c5e276e4e3054e8d397
lyd00 <yanda@vite.org>
2018-11-27 20:51:06 +0800

    Add benchmark that query account blocks

--- chain/benchmark/queryAccountBlock_test.go
+++ chain/benchmark/queryAccountBlock_test.go
@@ -19,13 +19,14 @@ func Benchmark_GetAccountBlocksByHash(b *testing.B) {
 	chainInstance := newChainInstance("insertAccountBlock", false)
 
 	const (
-		PARAMS_LENGTH = 1000
-		TRY_COUNT     = 10000
+		PARAMS_LENGTH = 10 * 10000
+		TRY_COUNT     = 50 * 10000
 
 		FORWARD_TRUE_PROBABILITY = 50
 		MAX_COUNT                = 500000
-		MIN_COUNT                = 200000
+		MIN_COUNT                = 1000
 	)
+	// create getAbsByHashParam
 	var params []getAbsByHashParam
 
 	allLatestBlock, _ := chainInstance.GetAllLatestAccountBlock()
@@ -97,4 +98,102 @@ func getAccountBlocksByHash(chainInstance chain.Chain, testParams []getAbsByHash
 
 	tps2.Stop()
 	tps2.Print()
+}
+
+func Benchmark_GetAccountBlockByHash(b *testing.B) {
+	chainInstance := newChainInstance("insertAccountBlock", false)
+
+	allLatestBlock, _ := chainInstance.GetAllLatestAccountBlock()
+
+	const (
+		QUERY_NUM_LIMIT        = 10000 * 10000
+		BLOCK_HASH_LIST_LENGTH = 10 * 10000
+		PRINT_PER_COUNT        = 1 * 10000
+	)
+
+	fmt.Printf("Create blockHashList...\n")
+	var blockHashList [BLOCK_HASH_LIST_LENGTH]types.Hash
+	for i := 0; i < BLOCK_HASH_LIST_LENGTH; i++ {
+		block := allLatestBlock[rand.Intn(len(allLatestBlock))]
+		addr := block.AccountAddress
+
+		randomHeight := rand.Uint64() % block.Height
+		if randomHeight <= 0 {
+			randomHeight = 1
+		}
+
+		randomBlock, _ := chainInstance.GetAccountBlockByHeight(&addr, randomHeight)
+
+		blockHashList[i] = randomBlock.Hash
+	}
+
+	fmt.Printf("Start query...\n")
+	tps := newTps(tpsOption{
+		name:          "GetAccountBlockByHash",
+		printPerCount: PRINT_PER_COUNT,
+	})
+
+	tps.Start()
+
+	for i := 0; i < QUERY_NUM_LIMIT; i++ {
+		hash := blockHashList[rand.Intn(BLOCK_HASH_LIST_LENGTH)]
+		_, err := chainInstance.GetAccountBlockByHash(&hash)
+		if err != nil {
+			b.Fatal(err)
+		}
+		tps.doOne()
+	}
+	tps.Stop()
+	tps.Print()
+}
+
+type getAbByHeightParam struct {
+	addr   types.Address
+	height uint64
+}
+
+func Benchmark_GetAccountBlockByHeight(b *testing.B) {
+	chainInstance := newChainInstance("insertAccountBlock", false)
+
+	allLatestBlock, _ := chainInstance.GetAllLatestAccountBlock()
+
+	const (
+		QUERY_NUM_LIMIT          = 10000 * 10000
+		BLOCK_HEIGHT_LIST_LENGTH = 1 * 10000
+		PRINT_PER_COUNT          = 1 * 10000
+	)
+
+	fmt.Printf("Create blockHeightList...\n")
+	var blockHeightList [BLOCK_HEIGHT_LIST_LENGTH]*getAbByHeightParam
+	for i := 0; i < BLOCK_HEIGHT_LIST_LENGTH; i++ {
+		block := allLatestBlock[rand.Intn(len(allLatestBlock))]
+		randomHeight := rand.Uint64() % block.Height
+		if randomHeight <= 0 {
+			randomHeight = 1
+		}
+
+		blockHeightList[i] = &getAbByHeightParam{
+			addr:   block.AccountAddress,
+			height: randomHeight,
+		}
+	}
+
+	fmt.Printf("Start query...\n")
+	tps := newTps(tpsOption{
+		name:          "GetAccountBlockByHeight",
+		printPerCount: PRINT_PER_COUNT,
+	})
+
+	tps.Start()
+
+	for i := 0; i < QUERY_NUM_LIMIT; i++ {
+		blockHeight := blockHeightList[rand.Intn(BLOCK_HEIGHT_LIST_LENGTH)]
+		_, err := chainInstance.GetAccountBlockByHeight(&blockHeight.addr, blockHeight.height)
+		if err != nil {
+			b.Fatal(err)
+		}
+		tps.doOne()
+	}
+	tps.Stop()
+	tps.Print()
 }
