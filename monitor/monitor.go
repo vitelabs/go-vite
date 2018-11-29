a023beff277b53b080eb383d0cfff54c78f2d84f
viteshan <viteshan@163.com>
2018-11-29 14:09:02 +0800

    remove monitor

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -2,10 +2,6 @@ package monitor
 
 import (
 	"encoding/json"
-	"math/rand"
-	"os"
-	"os/user"
-	"path"
 	"strconv"
 	"sync"
 	"sync/atomic"
@@ -17,26 +13,26 @@ import (
 )
 
 func init() {
-	usr, e := user.Current()
-	if e != nil {
-		log15.Root().Error("can't get current user.", "err", e)
-	}
-	PID := strconv.Itoa(os.Getpid())
-	//设置日志文件地址
-	dir := path.Join(usr.HomeDir, "go-vite", "backend-log")
-	os.MkdirAll(dir, os.ModePerm)
-
-	rand.Seed(time.Now().Unix())
-	fileName := path.Join(dir, "backend.log."+PID)
-
-	log15.Info("", "monitor-log", fileName)
-
-	logger = log15.New("logtype", "1", "appkey", "govite", "PID", PID)
-	logger.SetHandler(
-		log15.LvlFilterHandler(log15.LvlInfo, log15.Must.FileHandler(fileName, log15.JsonFormat())),
-	)
-	m = &monitor{r: newRing(60)}
-	go loop()
+	//usr, e := user.Current()
+	//if e != nil {
+	//	log15.Root().Error("can't get current user.", "err", e)
+	//}
+	//PID := strconv.Itoa(os.Getpid())
+	////设置日志文件地址
+	//dir := path.Join(usr.HomeDir, "go-vite", "backend-log")
+	//os.MkdirAll(dir, os.ModePerm)
+	//
+	//rand.Seed(time.Now().Unix())
+	//fileName := path.Join(dir, "backend.log."+PID)
+	//
+	//log15.Info("", "monitor-log", fileName)
+	//
+	//logger = log15.New("logtype", "1", "appkey", "govite", "PID", PID)
+	//logger.SetHandler(
+	//	log15.LvlFilterHandler(log15.LvlInfo, log15.Must.FileHandler(fileName, log15.JsonFormat())),
+	//)
+	//m = &monitor{r: newRing(60)}
+	//go loop()
 }
 
 var m *monitor
@@ -100,13 +96,13 @@ func LogDuration(t string, name string, duration int64) {
 }
 
 func log(t string, name string, i int64) {
-	k := key(t, name)
-	value, ok := m.ms.Load(k)
-	if ok {
-		value.(*Msg).add(i)
-	} else {
-		m.ms.Store(k, newMsg().add(i))
-	}
+	//k := key(t, name)
+	//value, ok := m.ms.Load(k)
+	//if ok {
+	//	value.(*Msg).add(i)
+	//} else {
+	//	m.ms.Store(k, newMsg().add(i))
+	//}
 }
 
 type stat struct {
