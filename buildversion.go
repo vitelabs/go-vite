7bbc7c26e1495fa47570e0e3326ad51e54967785
viteshan <viteshan@163.com>
2019-01-10 17:04:54 +0800

    release to v1.2.0

--- buildversion.go
+++ buildversion.go
@@ -1,4 +1,3 @@
 package govite
-
-const VITE_VERSION = "f6bc4a675255ad3a895cfd81eaeb21643bf0c70d"
-const VITE_BUILD_VERSION = "v1.1.2"
+const VITE_VERSION = "0d86fdcff7f40765259fa92dd5814e79251210bb"
+const VITE_BUILD_VERSION = "v1.2.0"
