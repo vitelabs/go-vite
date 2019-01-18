1ce8582e2ce6737603d87e7b682e66e62f5fa288
lyd00 <yanda@vite.org>
2019-01-18 13:30:33 +0800

    Fix recover

--- chain_db/database/db.go
+++ chain_db/database/db.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/opt"
 )
 
 //, &opt.Options{
@@ -10,10 +9,7 @@ import (
 //BlockSize:           2 * opt.KiB,
 //}
 func NewLevelDb(dbDir string) (*leveldb.DB, error) {
-	db, err := leveldb.OpenFile(dbDir, &opt.Options{
-		WriteBuffer:        64 * opt.MiB,
-		BlockCacheCapacity: 32 * opt.MiB,
-	})
+	db, err := leveldb.OpenFile(dbDir, nil)
 	if err != nil {
 		return nil, err
 	}
