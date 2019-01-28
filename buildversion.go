8b1c5f2cafa9ef628a4d9f5def4a0c9c7e0dbb48
jerry-vite <yinjun@vite.org>
2019-01-28 11:56:14 +0800

    v1.2.3-alpha.3

--- buildversion.go
+++ buildversion.go
@@ -1,3 +1,3 @@
 package govite
-const VITE_VERSION = "8baa47fdbb63b85fb8be8351ec3081dc693c4566"
-const VITE_BUILD_VERSION = "v1.2.3-alpha.2"
+const VITE_VERSION = "707becd887fc4a838b5656668fc880e112589dc0"
+const VITE_BUILD_VERSION = "v1.2.3-alpha.3"
