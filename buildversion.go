23dd013c2785aae4a95a931b93ac0be56330d2e7
vite-crzn <gexin@vite.org>
2019-01-30 16:29:05 +0800

    v1.2.3-alpha.4

--- buildversion.go
+++ buildversion.go
@@ -1,3 +1,4 @@
 package govite
-const VITE_VERSION = "707becd887fc4a838b5656668fc880e112589dc0"
-const VITE_BUILD_VERSION = "v1.2.3-alpha.3"
+
+const VITE_VERSION = "863d950858fd87d1232b157c1fc181bc62a88462"
+const VITE_BUILD_VERSION = "v1.2.3-alpha.4"
