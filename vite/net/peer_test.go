42300d1d37344702f6c6615c6fdf455c0fee18b6
jerry-vite <yinjun@vite.org>
2019-03-05 12:53:13 +0800

    peerSet store Peer, but not *peer

--- vite/net/peer_test.go
+++ vite/net/peer_test.go
@@ -37,14 +37,38 @@ func TestPeerSet_SyncPeer(t *testing.T) {
 
 func TestPeerSet_SyncPeer2(t *testing.T) {
 	var m = newPeerSet()
-	if m.SyncPeer() != nil {
+	var p Peer
+	if p = m.SyncPeer(); p != nil {
 		t.Fail()
 	}
 }
 
 func TestPeerSet_BestPeer(t *testing.T) {
 	var m = newPeerSet()
-	if m.BestPeer() != nil {
+	var p Peer
+	if p = m.BestPeer(); p != nil {
 		t.Fail()
 	}
+}
+
+func TestPeerSet_Get(t *testing.T) {
+	var m = newPeerSet()
+	var p Peer
+	if p = m.Get("hello"); p != nil {
+		t.Fail()
+	}
+}
+
+func ExamplePeerSet_Get2() {
+	var m = make(map[string]*peer)
+	var p Peer = m["hello"]
+	fmt.Println(p == nil)
+	// Output: false
+}
+
+func ExamplePeerSet_Get3() {
+	var m2 = make(map[string]Peer)
+	var p Peer = m2["hello"]
+	fmt.Println(p == nil)
+	// Output: true
 }
