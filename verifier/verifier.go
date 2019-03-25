0192bf8eec352028f1ddce36d5c065463011829f
vite-crzn <gexin@vite.org>
2019-03-25 15:53:54 +0800

    approve interface

--- verifier/verifier.go
+++ verifier/verifier.go
@@ -25,29 +25,29 @@ func (v *verifier) VerifyNetSnapshotBlock(block *ledger.SnapshotBlock) error {
 	return v.Sv.VerifyNetSb(block)
 }
 
-func (v *verifier) VerifyNetAccBlock(block ledger.AccountBlock) error {
+func (v *verifier) VerifyNetAccBlock(block *ledger.AccountBlock) error {
 	//todo 1. makesure genesis and initial-balance blocks don't need to check, return nil
 	//todo 2. block referred snapshot not arrive yet, return error
 	//3. VerifyHash
-	if err := v.Av.verifyHash(&block); err != nil {
+	if err := v.Av.verifyHash(block); err != nil {
 		return err
 	}
 	//4. VerifySignature
 	if block.IsReceiveBlock() || (len(block.Signature) > 0 || len(block.PublicKey) > 0) {
-		if err := v.Av.verifySignature(&block); err != nil {
+		if err := v.Av.verifySignature(block); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (v *verifier) VerifyPoolAccBlock(block ledger.AccountBlock, snapshotHash types.Hash) (*AccBlockPendingTask, *vm_db.VmAccountBlock, error) {
+func (v *verifier) VerifyPoolAccBlock(block *ledger.AccountBlock, snapshotHash *types.Hash) (*AccBlockPendingTask, *vm_db.VmAccountBlock, error) {
 	verifyResult, task, err := v.Av.verifyReferred(block)
 	switch verifyResult {
 	case PENDING:
 		return task, nil, nil
 	case SUCCESS:
-		blocks, err := v.Av.vmVerify(&block, &snapshotHash)
+		blocks, err := v.Av.vmVerify(block, snapshotHash)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -57,22 +57,22 @@ func (v *verifier) VerifyPoolAccBlock(block ledger.AccountBlock, snapshotHash ty
 	}
 }
 
-func (v *verifier) VerifyRPCAccBlock(block ledger.AccountBlock, snapshotHash types.Hash) (*vm_db.VmAccountBlock, error) {
+func (v *verifier) VerifyRPCAccBlock(block *ledger.AccountBlock, snapshotHash *types.Hash) (*vm_db.VmAccountBlock, error) {
 	if verifyResult, task, err := v.Av.verifyReferred(block); verifyResult != SUCCESS {
 		if err != nil {
 			return nil, err
 		}
 		return nil, errors.New("verify block failed, pending for:" + task.pendingHashListToStr())
 	}
-	return v.Av.vmVerify(&block, &snapshotHash)
+	return v.Av.vmVerify(block, snapshotHash)
 }
 
-func (v *verifier) VerifyAccBlockHash(block ledger.AccountBlock) error {
-	return v.Av.verifyHash(&block)
+func (v *verifier) VerifyAccBlockHash(block *ledger.AccountBlock) error {
+	return v.Av.verifyHash(block)
 }
 
-func (v *verifier) VerifyAccBlockSignature(block ledger.AccountBlock) error {
-	accType, err := v.Av.verifyAccAddress(&block)
+func (v *verifier) VerifyAccBlockSignature(block *ledger.AccountBlock) error {
+	accType, err := v.Av.verifyAccAddress(block)
 	if err != nil {
 		return err
 	}
@@ -88,42 +88,42 @@ func (v *verifier) VerifyAccBlockSignature(block ledger.AccountBlock) error {
 		if len(block.Signature) == 0 || len(block.PublicKey) == 0 {
 			return errors.New("signature or publicKey can't be nil")
 		}
-		if err := v.Av.verifySignature(&block); err != nil {
+		if err := v.Av.verifySignature(block); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (v *verifier) VerifyAccBlockNonce(block ledger.AccountBlock) error {
-	accType, err := v.Av.verifyAccAddress(&block)
+func (v *verifier) VerifyAccBlockNonce(block *ledger.AccountBlock) error {
+	accType, err := v.Av.verifyAccAddress(block)
 	if err != nil {
 		return err
 	}
 	if accType == AccountTypeNotSure {
 		return ErrVerifyAccountTypeNotSure
 	}
-	return v.Av.verifyNonce(&block, isAccTypeGeneral(accType))
+	return v.Av.verifyNonce(block, isAccTypeGeneral(accType))
 }
 
-func (v *verifier) VerifyAccBlockConfirmedTimes(block ledger.AccountBlock) error {
-	accType, err := v.Av.verifyAccAddress(&block)
+func (v *verifier) VerifyAccBlockConfirmedTimes(block *ledger.AccountBlock) error {
+	accType, err := v.Av.verifyAccAddress(block)
 	if err != nil {
 		return err
 	}
 	if accType == AccountTypeNotSure {
 		return ErrVerifyAccountTypeNotSure
 	}
-	return v.Av.verifyComfirmedTimes(&block, isAccTypeGeneral(accType))
+	return v.Av.verifyComfirmedTimes(block, isAccTypeGeneral(accType))
 }
 
-func (v *verifier) VerifyAccBlockProducerLegality(block ledger.AccountBlock) error {
-	accType, err := v.Av.verifyAccAddress(&block)
+func (v *verifier) VerifyAccBlockProducerLegality(block *ledger.AccountBlock) error {
+	accType, err := v.Av.verifyAccAddress(block)
 	if err != nil {
 		return err
 	}
 	if accType == AccountTypeNotSure {
 		return ErrVerifyAccountTypeNotSure
 	}
-	return v.Av.verifyProducerLegality(&block, isAccTypeGeneral(accType))
+	return v.Av.verifyProducerLegality(block, isAccTypeGeneral(accType))
 }
