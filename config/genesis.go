3b07ff9ac17b7deee50e32ea2d2a2308484382e5
viteLiz <chenping@vite.org>
2019-03-29 17:45:19 +0800

    delete unused code

--- config/genesis.go
+++ config/genesis.go
@@ -5,33 +5,6 @@ import (
 	"math/big"
 )
 
-type ConditionRegisterData struct {
-	PledgeAmount *big.Int
-	PledgeToken  types.TokenTypeId
-	PledgeHeight uint64
-}
-
-type VoteConditionData struct {
-	Amount  *big.Int
-	TokenId types.TokenTypeId
-}
-
-type ConsensusGroupInfo struct {
-	NodeCount              uint8
-	Interval               int64
-	PerCount               int64
-	RandCount              uint8
-	RandRank               uint8
-	CountingTokenId        types.TokenTypeId
-	RegisterConditionId    uint8
-	RegisterConditionParam ConditionRegisterData
-	VoteConditionId        uint8
-	VoteConditionParam     VoteConditionData
-	Owner                  types.Address
-	PledgeAmount           *big.Int
-	WithdrawHeight         uint64
-}
-
 type ForkPoint struct {
 	Height uint64
 	Hash   *types.Hash
