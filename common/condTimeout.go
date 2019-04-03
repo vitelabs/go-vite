7f6765c2aeb1cfd9c90342956c32150044d7ab23
viteshan <viteshan@163.com>
2019-04-03 14:56:41 +0800

    refactor condTimeout

--- common/condTimeout.go
+++ common/condTimeout.go
@@ -2,48 +2,63 @@ package common
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 
-	"github.com/go-errors/errors"
+	"github.com/pkg/errors"
 )
 
 var TimeoutErr = errors.New("timeout")
 
 type TimeoutCond struct {
-	cd *sync.Cond
+	notifyNum uint32
+	L         sync.Locker
+	signal    chan uint8
 }
 
 func NewTimeoutCond() *TimeoutCond {
 	mutex := &sync.Mutex{}
-	return &TimeoutCond{cd: sync.NewCond(mutex)}
+	return &TimeoutCond{L: mutex, signal: make(chan uint8, 0)}
 }
 
 func (self *TimeoutCond) Wait() {
-	self.cd.L.Lock()
-	defer self.cd.L.Unlock()
-	self.cd.Wait()
+	old := atomic.SwapUint32(&self.notifyNum, 0)
+	if old > 0 {
+		return
+	}
+	ch := self.signal
+	select {
+	case <-ch:
+		return
+	}
 }
 
 func (self *TimeoutCond) WaitTimeout(t time.Duration) error {
-	done := make(chan struct{})
-	go func() {
-		self.Wait()
-		close(done)
-	}()
-	select {
-	case <-time.After(t):
-		// timed out
-		return TimeoutErr
-	case <-done:
-		// Wait returned
+	old := atomic.SwapUint32(&self.notifyNum, 0)
+	if old > 0 {
 		return nil
+	}
+	ch := self.signal
+	select {
+	case <-ch:
+		return nil
+	case <-time.After(t):
+		//fmt.Println(time.Now())
+		return TimeoutErr
 	}
 }
 
 func (self *TimeoutCond) Broadcast() {
-	self.cd.Broadcast()
-
+	atomic.AddUint32(&self.notifyNum, 1)
+	self.L.Lock()
+	defer self.L.Unlock()
+	close(self.signal)
+	self.signal = make(chan uint8, 0)
 }
 func (self *TimeoutCond) Signal() {
-	self.cd.Signal()
+	atomic.AddUint32(&self.notifyNum, 1)
+	select {
+	case self.signal <- uint8(1):
+	default:
+	}
 }
