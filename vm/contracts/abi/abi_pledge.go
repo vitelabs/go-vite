814b219b82c2332141e271aff24d0546a7a4172b
weichaolee <weichao@vite.org>
2019-04-09 19:44:50 +0800

    fix for abi_pledge conflict

--- vm/contracts/abi/abi_pledge.go
+++ vm/contracts/abi/abi_pledge.go
@@ -53,10 +53,6 @@ func GetBeneficialFromPledgeKey(key []byte) types.Address {
 	address, _ := types.BytesToAddress(key[types.AddressSize:])
 	return address
 }
-func GetPledgeAddrFromPledgeKey(key []byte) types.Address {
-	address, _ := types.BytesToAddress(key[:types.AddressSize])
-	return address
-}
 
 func GetPledgeBeneficialAmount(db StorageDatabase, beneficial types.Address) *big.Int {
 	key := GetPledgeBeneficialKey(beneficial)
