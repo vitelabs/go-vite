2d0b1f747f9b1bd0f8e021304b15e1d1bd6e62c0
weichaolee <weichao@vite.org>
2019-04-10 13:45:29 +0800

    Revert "fix for abi_pledge conflict"
    This reverts commit 814b219b82c2332141e271aff24d0546a7a4172b.

--- vm/contracts/abi/abi_pledge.go
+++ vm/contracts/abi/abi_pledge.go
@@ -53,6 +53,10 @@ func GetBeneficialFromPledgeKey(key []byte) types.Address {
 	address, _ := types.BytesToAddress(key[types.AddressSize:])
 	return address
 }
+func GetPledgeAddrFromPledgeKey(key []byte) types.Address {
+	address, _ := types.BytesToAddress(key[:types.AddressSize])
+	return address
+}
 
 func GetPledgeBeneficialAmount(db StorageDatabase, beneficial types.Address) *big.Int {
 	key := GetPledgeBeneficialKey(beneficial)
