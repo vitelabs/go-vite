e2b4ecc3b56d0122a3c4b1a033e7b27d8718d4d4
viteshan <viteshan@163.com>
2019-04-24 16:00:27 +0800

    update vite address

--- common/types/address.go
+++ common/types/address.go
@@ -8,6 +8,8 @@ import (
 	"math/big"
 	"strings"
 
+	"github.com/go-errors/errors"
+
 	"github.com/vitelabs/go-vite/common/helper"
 	vcrypto "github.com/vitelabs/go-vite/crypto"
 	"github.com/vitelabs/go-vite/crypto/ed25519"
@@ -15,16 +17,24 @@ import (
 
 const (
 	AddressPrefix       = "vite_"
-	AddressSize         = 20
+	AddressSize         = 21
+	AddressCoreSize     = 20
 	addressChecksumSize = 5
 	addressPrefixLen    = len(AddressPrefix)
-	hexAddressLength    = addressPrefixLen + 2*AddressSize + 2*addressChecksumSize
+	hexAddrCoreLen      = 2 * AddressCoreSize
+	hexAddrChecksumLen  = 2 * addressChecksumSize
+	hexAddressLength    = addressPrefixLen + hexAddrCoreLen + hexAddrChecksumLen
+)
+
+const (
+	UserAddrByte     = byte(0)
+	ContractAddrByte = byte(1)
 )
 
 var (
-	AddressPledge, _         = BytesToAddress([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3})
-	AddressConsensusGroup, _ = BytesToAddress([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4})
-	AddressMintage, _        = BytesToAddress([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5})
+	AddressPledge, _         = BytesToAddress([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, ContractAddrByte})
+	AddressConsensusGroup, _ = BytesToAddress([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, ContractAddrByte})
+	AddressMintage, _        = BytesToAddress([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, ContractAddrByte})
 
 	BuiltinContractAddrList             = []Address{AddressPledge, AddressConsensusGroup, AddressMintage}
 	BuiltinContractWithoutQuotaAddrList = []Address{AddressPledge, AddressConsensusGroup, AddressMintage}
@@ -80,12 +90,7 @@ func BigToAddress(b *big.Int) (Address, error) {
 }
 
 func HexToAddress(hexStr string) (Address, error) {
-	if IsValidHexAddress(hexStr) {
-		addr, _ := getAddressFromHex(hexStr)
-		return addr, nil
-	} else {
-		return Address{}, fmt.Errorf("not valid hex address %v", hexStr)
-	}
+	return ValidHexAddress(hexStr)
 }
 
 func HexToAddressPanic(hexstr string) Address {
@@ -97,29 +102,43 @@ func HexToAddressPanic(hexstr string) Address {
 }
 
 func IsValidHexAddress(hexStr string) bool {
+	_, err := ValidHexAddress(hexStr)
+	return err == nil
+}
+
+func ValidHexAddress(hexStr string) (Address, error) {
 	if len(hexStr) != hexAddressLength || !strings.HasPrefix(hexStr, AddressPrefix) {
-		return false
+		return Address{}, errors.New("error hex address or prefix")
 	}
 
-	address, err := getAddressFromHex(hexStr)
+	addrCore, err := getAddrCoreFromHex(hexStr)
 	if err != nil {
-		return false
+		return Address{}, err
 	}
 
 	addressChecksum, err := getAddressChecksumFromHex(hexStr)
 	if err != nil {
-		return false
+		return Address{}, err
 	}
 
-	if !bytes.Equal(vcrypto.Hash(addressChecksumSize, address[:]), addressChecksum[:]) {
-		return false
+	checksum := vcrypto.Hash(addressChecksumSize, addrCore[:])
+
+	if bytes.Equal(checksum, addressChecksum[:]) {
+		return GenUserAddress(addrCore[:])
 	}
 
-	return true
+	if bytes.Equal(checksum, helper.LDI(addressChecksum[:])) {
+		return GenContractAddress(addrCore[:])
+	}
+	return Address{}, errors.Errorf("error address[%s] checksum", hexStr)
 }
 
 func PubkeyToAddress(pubkey []byte) Address {
-	addr, _ := BytesToAddress(vcrypto.Hash(AddressSize, pubkey))
+	hash := vcrypto.Hash(AddressCoreSize, pubkey)
+	addr, err := GenUserAddress(hash)
+	if err != nil {
+		panic(err)
+	}
 	return addr
 }
 
@@ -136,7 +155,16 @@ func (addr *Address) SetBytes(b []byte) error {
 }
 
 func (addr Address) Hex() string {
-	return AddressPrefix + hex.EncodeToString(addr[:]) + hex.EncodeToString(vcrypto.Hash(addressChecksumSize, addr[:]))
+	coreAddr := addr[:AddressCoreSize]
+	byt := addr[AddressCoreSize]
+	if byt == UserAddrByte {
+		return AddressPrefix + hex.EncodeToString(coreAddr) + hex.EncodeToString(vcrypto.Hash(addressChecksumSize, coreAddr))
+	} else if byt == ContractAddrByte {
+		hash := vcrypto.Hash(addressChecksumSize, coreAddr)
+		return AddressPrefix + hex.EncodeToString(coreAddr) + hex.EncodeToString(helper.XOR(hash))
+	} else {
+		return fmt.Sprintf("error address[%d]", byt)
+	}
 }
 func (addr Address) Bytes() []byte { return addr[:] }
 func (addr Address) String() string {
@@ -153,6 +181,20 @@ func CreateContractAddress(data ...[]byte) Address {
 	return addr
 }
 
+func GenContractAddress(data []byte) (Address, error) {
+	var addr []byte
+	addr = append(addr, data[:]...)
+	addr = append(addr, ContractAddrByte)
+	return BytesToAddress(addr[:])
+}
+
+func GenUserAddress(data []byte) (Address, error) {
+	var addr []byte
+	addr = append(addr, data[:]...)
+	addr = append(addr, UserAddrByte)
+	return BytesToAddress(addr[:])
+}
+
 func CreateAddressWithDeterministic(d [32]byte) (Address, ed25519.PrivateKey, error) {
 	pub, pri, error := ed25519.GenerateKeyFromD(d)
 	return PubkeyToAddress(pub), pri, error
@@ -160,13 +202,19 @@ func CreateAddressWithDeterministic(d [32]byte) (Address, ed25519.PrivateKey, er
 
 func getAddressFromHex(hexStr string) ([AddressSize]byte, error) {
 	var b [AddressSize]byte
-	_, err := hex.Decode(b[:], []byte(hexStr[addressPrefixLen:2*AddressSize+addressPrefixLen]))
+	_, err := hex.Decode(b[:], []byte(hexStr[addressPrefixLen:hexAddrCoreLen+addressPrefixLen]))
+	return b, err
+}
+
+func getAddrCoreFromHex(hexStr string) ([AddressCoreSize]byte, error) {
+	var b [AddressCoreSize]byte
+	_, err := hex.Decode(b[:], []byte(hexStr[addressPrefixLen:hexAddrCoreLen+addressPrefixLen]))
 	return b, err
 }
 
 func getAddressChecksumFromHex(hexStr string) ([addressChecksumSize]byte, error) {
 	var b [addressChecksumSize]byte
-	_, err := hex.Decode(b[:], []byte(hexStr[2*AddressSize+addressPrefixLen:]))
+	_, err := hex.Decode(b[:], []byte(hexStr[hexAddrCoreLen+addressPrefixLen:]))
 	return b, err
 }
 
@@ -174,7 +222,8 @@ func (a *Address) UnmarshalJSON(input []byte) error {
 	if !isString(input) {
 		return ErrJsonNotString
 	}
-	addresses, e := HexToAddress(string(trimLeftRightQuotation(input)))
+
+	addresses, e := HexToAddress(strings.Trim(string(input), "\""))
 	if e != nil {
 		return e
 	}
