d91788210ff6d1e53bc6135bb240f7bc42bb17d8
jerry-vite <yinjun@vite.org>
2019-04-25 17:50:10 +0800

    compress log

--- p2p/codec.go
+++ p2p/codec.go
@@ -277,6 +277,7 @@ func (t *transport) WriteMsg(msg Msg) (err error) {
 	var compress bool
 	payloadLen := len(msg.Payload)
 	beforeCompress := time.Now()
+	beforeCompressLen := payloadLen
 	if payloadLen > t.minCompressLength {
 		payloadCompressed := bytes_pool.Get(snappy.MaxEncodedLen(payloadLen))
 		payloadCompressed = snappy.Encode(payloadCompressed, msg.Payload)
@@ -291,7 +292,7 @@ func (t *transport) WriteMsg(msg Msg) (err error) {
 			bytes_pool.Put(payloadCompressed)
 		}
 	}
-	p2pLog.Debug(fmt.Sprintf("compress %d bytes to %d bytes, ellapse %s", payloadLen, len(msg.Payload), time.Now().Sub(beforeCompress)))
+	p2pLog.Debug(fmt.Sprintf("compress %d bytes to %d bytes, ellapse %s", beforeCompressLen, payloadLen, time.Now().Sub(beforeCompress)))
 
 	// store msg length
 	lsize := PutVarint(head[headLen:], uint(payloadLen))
