8c6b3ea7efcff5c1ad2388e3bad32950bbf23765
lyd00 <yanda@vite.org>
2019-05-15 15:47:59 +0800

    Modify initCache

--- chain/state/redo.go
+++ chain/state/redo.go
@@ -87,22 +87,27 @@ func NewStorageRedo(chain Chain, chainDir string) (*Redo, error) {
 		log: log15.New("module", "state_redo"),
 	}
 
-	redo.initCache()
+	if err := redo.initCache(); err != nil {
+		redo.log.Warn("redo.initCache failed, Error: %s", err, "method", "NewStorageRedo")
+	}
 
 	store.RegisterAfterRecover(func() {
-		redo.initCache()
+		redo.log.Info("after recover, redo.initCache()")
+		if err := redo.initCache(); err != nil {
+			panic(errors.New(fmt.Sprintf("after recover, redo.initCache failed, Error: %s", err.Error())))
+		}
 	})
 	return redo, nil
 }
 
 // init
-func (redo *Redo) initCache() {
+func (redo *Redo) initCache() error {
 
 	height := uint64(0)
 
 	latestSnapshotBlock, err := redo.chain.QueryLatestSnapshotBlock()
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if latestSnapshotBlock != nil {
 		height = latestSnapshotBlock.Height
