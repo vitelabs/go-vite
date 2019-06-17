8638c02d63e4a45fe43a075feea7f8821892ce9b
viteLiz <chenping@vite.org>
2019-06-17 11:47:31 +0800

    updated random test

--- generator/utils_test.go
+++ generator/utils_test.go
@@ -25,7 +25,7 @@ func TestSeed(t *testing.T) {
 	chain := mockChain{seed: seed}
 	vmGlobalStatus := NewVMGlobalStatus(chain, nil, types.Hash{})
 	for i := 0; i < 100; i++ {
-		rand, err := vmGlobalStatus.Seed(1)
+		rand, err := vmGlobalStatus.Seed()
 		if err != nil || rand != seed {
 			t.Fatalf("before hard fork, expected [nil, %v], got [%v, %v]", seed, err, rand)
 		}
@@ -36,7 +36,7 @@ func TestSeed(t *testing.T) {
 	vmGlobalStatus = NewVMGlobalStatus(chain, nil, types.Hash{})
 	randList := []uint64{3859229079807094838, 12152911693104629581, 9644037654042465595, 9243194358276174014, 11414992830392538450, 3925454220078744999, 16503802664012760397, 7980644548622968793, 13268626495317126268, 12067214392073868583}
 	for index, expected := range randList {
-		rand, err := vmGlobalStatus.Seed(111)
+		rand, err := vmGlobalStatus.Random()
 		if err != nil || rand != expected {
 			t.Fatalf("after hard fork, index %v, expected [nil, %v], got [%v, %v]", index, expected, err, rand)
 		}
