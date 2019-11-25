7df941b3c7102d58d1a4d6dbaacf0a1475bbd78a
weichaolee <weichao@vite.org>
2019-11-25 18:26:40 +0800

    build version

--- buildversion.go
+++ buildversion.go
@@ -1,3 +1,3 @@
 package govite
-const VITE_VERSION = "5e18b5504c67b8c9ce32a8a8c01244d2d4e3daa9"
-const VITE_BUILD_VERSION = "v2.6.2"
+const VITE_VERSION = "ada40ca91b9699f248fe6072b40686691b634670"
+const VITE_BUILD_VERSION = "v2.6.3"
