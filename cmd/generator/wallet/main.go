1ea425b4c9d0e849bd061cdf2184e99c61c23b3a
viteshan <viteshan@163.com>
2020-05-19 16:35:38 +0800

    update wallet

--- cmd/generator/wallet/main.go
+++ cmd/generator/wallet/main.go
@@ -4,8 +4,7 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/tyler-smith/go-bip39"
-	"github.com/vitelabs/go-vite/wallet/hd-bip/derivation"
+	"github.com/vitelabs/go-vite/wallet"
 )
 
 var num = flag.Int("num", 1, "num ")
@@ -13,27 +12,10 @@ var num = flag.Int("num", 1, "num ")
 func main() {
 	flag.Parse()
 	for i := 0; i < *num; i++ {
-		entropy, err := bip39.NewEntropy(256)
+		addr, key, mnemonic, err := wallet.RandomMnemonic24()
 		if err != nil {
 			panic(err)
 		}
-		mnemonic, err := bip39.NewMnemonic(entropy)
-		if err != nil {
-			panic(err)
-		}
-		seed := bip39.NewSeed(mnemonic, "")
-		key, err := derivation.DeriveWithIndex(0, seed)
-		if err != nil {
-			panic(err)
-		}
-		address, err := key.Address()
-		if err != nil {
-			panic(err)
-		}
-		priKeys, err := key.PrivateKey()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s,%s,%s\n", address, priKeys.Hex(), mnemonic)
+		fmt.Printf("address:%s, key:%s, mnemonic:%s\n", addr, key.Hex(), mnemonic)
 	}
 }
---
+++ wallet/wallet.go
@@ -0,0 +1,45 @@
+package wallet
+
+import (
+	"github.com/cosmos/go-bip39"
+	"github.com/vitelabs/go-vite/common/types"
+	"github.com/vitelabs/go-vite/crypto/ed25519"
+	"github.com/vitelabs/go-vite/wallet/hd-bip/derivation"
+)
+
+func RandomMnemonic24() (types.Address, ed25519.PrivateKey, string, error) {
+	entropy, err := bip39.NewEntropy(256)
+	if err != nil {
+		return types.Address{}, nil, "", err
+	}
+	return newMnemonic(entropy)
+}
+
+func RandomMnemonic12() (types.Address, ed25519.PrivateKey, string, error) {
+	entropy, err := bip39.NewEntropy(128)
+	if err != nil {
+		return types.Address{}, nil, "", err
+	}
+	return newMnemonic(entropy)
+}
+
+func newMnemonic(entropy []byte) (types.Address, ed25519.PrivateKey, string, error) {
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		return types.Address{}, nil, "", err
+	}
+	seed := bip39.NewSeed(mnemonic, "")
+	key, err := derivation.DeriveWithIndex(0, seed)
+	if err != nil {
+		return types.Address{}, nil, "", err
+	}
+	address, err := key.Address()
+	if err != nil {
+		return types.Address{}, nil, "", err
+	}
+	priKeys, err := key.PrivateKey()
+	if err != nil {
+		return types.Address{}, nil, "", err
+	}
+	return *address, priKeys, mnemonic, nil
+}
