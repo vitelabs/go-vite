5ac146b4a7d153a6e3920b615b550956a7ecb188
weichaolee <weichao@vite.org>
2020-06-10 21:26:02 +0800

    refactory for duper support snapshotHeight state

--- rpcapi/api/dumper.go
+++ rpcapi/api/dumper.go
@@ -30,72 +30,68 @@ func (f Dumper) String() string {
 	return "DumperApi"
 }
 
-func (f Dumper) DumpBalance(token types.TokenTypeId, threshold *big.Int) (error) {
+func (f Dumper) DumpBalance(token types.TokenTypeId, snapshotHeight uint64, accountBlockHash *types.Hash) (error) {
+	var snapshotBlock *ledger.SnapshotBlock
+	var err error
+	if snapshotBlock, err = f.chain.GetSnapshotBlockByHeight(snapshotHeight); err != nil {
+		return err
+	}
 	res := make(map[types.Address]*big.Int, 100)
 	f.chain.IterateAccounts(func(addr types.Address, accountId uint64, err1 error) bool {
 		if err1 != nil {
+			f.log.Error("GetLatestAccountBlock IterateAccounts failed, error is "+err.Error(), "method", "DumpBalance")
 			return false
 		}
-		if balance, err2 := f.chain.GetBalance(addr, token); err2 == nil {
-			if balance != nil && balance.Sign() > 0 {
-				res[addr] = balance
-			}
-		} else {
-			f.log.Error("GetLatestAccountBlock failed, error is "+err2.Error(), "method", "DumpBalance")
+		if balances, err2 := f.chain.GetConfirmedBalanceList([]types.Address{addr}, token, snapshotBlock.Hash); err2 != nil {
+			f.log.Error("GetLatestAccountBlock GetConfirmedBalanceList failed, error is "+err2.Error(), "method", "DumpBalance")
 			return false
+		} else if balance, ok := balances[addr]; ok && balance.Sign() > 0 {
+			res[addr] = balance
 		}
 		return true
 	})
-	db, err := getVmDb(f.chain, types.AddressDexFund)
-	if err != nil {
-		return err
-	}
-	iterator, err := db.NewStorageIterator([]byte("fd:"))
-	if err != nil {
-		return err
-	}
-	defer iterator.Release()
-	var userAccountValue []byte
+	pageSize := 10
+	startAddress := types.ZERO_ADDRESS
 	for {
-		if ok := iterator.Next(); ok {
-			userAccountValue = iterator.Value()
-			if len(userAccountValue) == 0 {
-				continue
-			}
-		} else {
-			break
-		}
-		userFund := &dex.Fund{}
-		if err = userFund.DeSerialize(userAccountValue); err != nil {
+		if funds, err := f.chain.GetDexFundsByPage(snapshotBlock.Hash, startAddress, pageSize); err != nil {
 			return err
-		}
-		for _, acc := range userFund.Accounts {
-			if bytes.Equal(acc.Token, token.Bytes()) {
-				dexAmt := dex.AddBigInt(acc.Available, acc.Locked)
-				if token == dex.VxTokenId {
-					vxLocked := dex.AddBigInt(acc.VxLocked, acc.VxUnlocking)
-					dexAmt = dex.AddBigInt(dexAmt, vxLocked)
-				} else if token == ledger.ViteTokenId && len(acc.CancellingStake) > 0 {
-					dexAmt = dex.AddBigInt(dexAmt, acc.CancellingStake)
+		} else {
+			for _, fund := range funds {
+				for _, acc := range fund.Accounts {
+					if bytes.Equal(acc.Token, token.Bytes()) {
+						dexAmt := dex.AddBigInt(acc.Available, acc.Locked)
+						if token == dex.VxTokenId {
+							vxLocked := dex.AddBigInt(acc.VxLocked, acc.VxUnlocking)
+							dexAmt = dex.AddBigInt(dexAmt, vxLocked)
+						} else if token == ledger.ViteTokenId && len(acc.CancellingStake) > 0 {
+							dexAmt = dex.AddBigInt(dexAmt, acc.CancellingStake)
+						}
+						if len(dexAmt) == 0 {
+							continue
+						}
+						address, _ := types.BytesToAddress(fund.Address)
+						if accAmt, ok := res[address]; ok {
+							res[address] = new(big.Int).Add(accAmt, new(big.Int).SetBytes(dexAmt))
+						} else {
+							res[address] = new(big.Int).SetBytes(dexAmt)
+						}
+					}
 				}
-				if len(dexAmt) == 0 {
-					continue
-				}
-				address, _ := types.BytesToAddress(userFund.Address)
-				if accAmt, ok := res[address]; ok {
-					res[address] = new(big.Int).Add(accAmt, new(big.Int).SetBytes(dexAmt))
-				} else {
-					res[address] = new(big.Int).SetBytes(dexAmt)
-				}
+			}
+
+			fundsLen := len(funds)
+			if fundsLen < pageSize {
+				break
+			}
+			if startAddress, err = types.BytesToAddress(funds[fundsLen-1].Address); err != nil {
+				return err
 			}
 		}
 	}
 	var validNum = 0
 	for k, v := range res {
-		if v.Cmp(threshold) > 0 {
-			fmt.Printf("%s,%s\n", k.String(), v.String())
-			validNum++
-		}
+		fmt.Printf("%s,%s\n", k.String(), v.String())
+		validNum++
 	}
 	fmt.Printf(">>>>>>>>>>>>>>>>>>>>> valid size %d\n", validNum)
 	return nil
