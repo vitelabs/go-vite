682874dc36f09ae344f0d365dd868f43e62a7a3f
viteshan <wangjie@vite.org>
2020-11-26 17:24:02 +0800

    add account blocks sort for blocksDB

--- chain/block/account_block.go
+++ chain/block/account_block.go
@@ -3,6 +3,8 @@ package chain_block
 import (
 	"errors"
 	"fmt"
+	"sort"
+
 	"github.com/vitelabs/go-vite/chain/file_manager"
 	"github.com/vitelabs/go-vite/ledger"
 )
@@ -19,4 +21,22 @@ func (bDB *BlockDB) GetAccountBlock(location *chain_file_manager.Location) (*led
 	}
 
 	return ab, nil
+}
+
+func sortAccountBlocksInChunk(chunk *ledger.SnapshotChunk) []*ledger.AccountBlock {
+	if len(chunk.AccountBlocks) == 0 {
+		return chunk.AccountBlocks
+	}
+	result := make([]*ledger.AccountBlock, len(chunk.AccountBlocks))
+	copy(result, chunk.AccountBlocks)
+
+	sort.Slice(result, func(i, j int) bool {
+		addressResult := result[i].AccountAddress.Compare(result[j].AccountAddress)
+		if addressResult == 0 {
+			return result[i].Height < result[j].Height
+		} else {
+			return addressResult < 0
+		}
+	})
+	return result
 }
