62538483880a327069b44c14bd01646f66103328
viteshan <wangjie@vite.org>
2020-11-30 11:54:22 +0800

    add index printer

---
+++ cmd/printer/index/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/vitelabs/go-vite/common/helper"
+	"github.com/vitelabs/go-vite/common/types"
+	chain_index "github.com/vitelabs/go-vite/ledger/chain/index"
+)
+
+//  env GOOS=linux GOARCH=amd64 go build -i -o index_printer ./cmd/printer/index/main.go
+
+var hash = flag.String("hash", "", "hash")
+var dir = flag.String("dataDir", "devdata", "data dir, for example: devdata")
+
+func main() {
+	flag.Parse()
+	db, err := chain_index.NewIndexDB(*dir)
+
+	helper.AssertNil(err)
+	hash := types.HexToHashPanic(*hash)
+	location, err := db.GetAccountBlockLocationByHash(&hash)
+	helper.AssertNil(err)
+	fmt.Println("location", location.FileId, location.Offset)
+}
