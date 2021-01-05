315a8d8822933419817de094c184e55d51320d51
viteshan <wangjie@vite.org>
2021-01-05 11:34:14 +0800

    update version from 2.10.0 to 2.10.1

--- ledger/chain/index/index_db_test.go
+++ ledger/chain/index/index_db_test.go
@@ -4,8 +4,7 @@ import (
 	"path"
 	"testing"
 
-	"gotest.tools/assert"
-
+	"github.com/stretchr/testify/assert"
 	"github.com/vitelabs/go-vite/common"
 	"github.com/vitelabs/go-vite/common/types"
 )
@@ -13,13 +12,13 @@ import (
 func TestDumpFileLocation(t *testing.T) {
 	chainDir := path.Join(common.HomeDir(), ".gvite/mockdata/ledger")
 	db, err := NewIndexDB(chainDir)
-	assert.NilError(t, err)
+	assert.NoError(t, err)
 	step := uint64(75 * 10)
 	from := types.GenesisHeight
 
 	for i := from; ; i = i + step {
 		location, err := db.GetSnapshotBlockLocation(i)
-		assert.NilError(t, err)
+		assert.NoError(t, err)
 		if location == nil {
 			break
 		}
@@ -30,11 +29,12 @@ func TestDumpFileLocation(t *testing.T) {
 func TestIndexDB_GetLatestAccountBlock(t *testing.T) {
 	chainDir := path.Join(common.HomeDir(), ".gvite/mockdata/ledger")
 	db, err := NewIndexDB(chainDir)
-	assert.NilError(t, err)
-	address, err := types.HexToAddress("vite_cfe352f1778a77d6ee685182f2d15c4c4c1367ea612593171c")
-	assert.NilError(t, err)
+	assert.NoError(t, err)
+	address, err := types.HexToAddress("vite_7c8c9e1e878e8a6ddf59c66a83791a5755a8fcf606c4bd31ea")
+	assert.NoError(t, err)
 	height, location, err := db.GetLatestAccountBlock(&address)
-	assert.NilError(t, err)
+	assert.NoError(t, err)
+	assert.NotNil(t, location)
 
 	t.Log(height, location.FileId, location.Offset)
 }
