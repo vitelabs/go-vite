398df7acd7980733765823455bf2be25f0e96222
viteshan <wangjie@vite.org>
2021-01-06 15:37:50 +0800

    update

--- rpcapi/api/debug.go
+++ rpcapi/api/debug.go
@@ -6,3 +6,11 @@ type Debug struct {
 func NewDebug() *Debug {
 	return &Debug{}
 }
+
+func (p Debug) String() string {
+	return "DebugApi"
+}
+
+func (p *Debug) Hello() (string, error) {
+	return "hello world", nil
+}
