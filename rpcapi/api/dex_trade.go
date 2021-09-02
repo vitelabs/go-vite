053e070e91b3b0946630e15226331a42849b989d
viteshan <wangjie@vite.org>
2021-09-02 15:15:53 +0800

    fix: dextrade rpc

--- rpcapi/api/dex_trade.go
+++ rpcapi/api/dex_trade.go
@@ -76,7 +76,16 @@ func (f DexTradeApi) GetOrdersFromMarket(tradeToken, quoteToken types.TokenTypeI
 	}
 }
 
-func (f DexTradeApi) GetAllOrdersFromMarket(tradeToken, quoteToken types.TokenTypeId, begin, end int) (ordersRes *apidex.OrdersRes, err error) {
+type MarketOrderParam struct {
+	TradeToken types.TokenTypeId
+	QuoteToken types.TokenTypeId
+	SellBegin  int
+	SellEnd    int
+	BuyBegin   int
+	BuyEnd     int
+}
+
+func (f DexTradeApi) GetMarketOrders(param MarketOrderParam) (ordersRes *apidex.OrdersRes, err error) {
 	if fundDb, err := getVmDb(f.chain, types.AddressDexFund); err != nil {
 		return nil, err
 	} else {
@@ -84,29 +93,34 @@ func (f DexTradeApi) GetAllOrdersFromMarket(tradeToken, quoteToken types.TokenTy
 		if err != nil {
 			return nil, err
 		}
-		if marketInfo, ok := dex.GetMarketInfo(fundDb, tradeToken, quoteToken); !ok {
+		if marketInfo, ok := dex.GetMarketInfo(fundDb, param.TradeToken, param.QuoteToken); !ok {
 			return nil, dex.TradeMarketNotExistsErr
 		} else {
 			if tradeDb, err := getVmDb(f.chain, types.AddressDexTrade); err != nil {
 				return nil, err
 			} else {
-				matcher := dex.NewMatcherWithMarketInfo(tradeDb, marketInfo)
-				sellOds, sellSize, err := matcher.GetOrdersFromMarket(true, begin, end)
-				if err != nil {
-					return nil, err
-				}
-				buyOds, buySize, err := matcher.GetOrdersFromMarket(false, begin, end)
-				if err != nil {
-					return nil, err
-				}
 				var obs []*apidex.RpcOrder
-				obs = append(obs, apidex.OrdersToRpc(sellOds)...)
-				obs = append(obs, apidex.OrdersToRpc(buyOds)...)
+				matcher := dex.NewMatcherWithMarketInfo(tradeDb, marketInfo)
+				if param.SellEnd > param.SellBegin {
+					sellOds, _, err := matcher.GetOrdersFromMarket(true, param.SellBegin, param.SellEnd)
+					if err != nil {
+						return nil, err
+					}
+					obs = append(obs, apidex.OrdersToRpc(sellOds)...)
+				}
+
+				if param.BuyEnd > param.BuyBegin {
+					buyOds, _, err := matcher.GetOrdersFromMarket(false, param.BuyBegin, param.BuyEnd)
+					if err != nil {
+						return nil, err
+					}
+					obs = append(obs, apidex.OrdersToRpc(buyOds)...)
+				}
 				latest2, err := f.chain.GetLatestAccountBlock(types.AddressDexTrade)
 				if err != nil {
 					return nil, err
 				}
-				return &apidex.OrdersRes{obs, buySize + sellSize, latest.HashHeight(), latest2.HashHeight()}, nil
+				return &apidex.OrdersRes{obs, len(obs), latest.HashHeight(), latest2.HashHeight()}, nil
 			}
 		}
 	}
