11a9627bc0eedfe2c566a756ce79357baf88d86d
charles-liu <charles@vite.org>
2022-01-22 19:51:54 +0800

    feat: add contract_query RPC

--- version/buildversion.go
+++ version/buildversion.go
@@ -1,3 +1,3 @@
 package version
-const VITE_COMMIT_VERSION = "7c37ad5788a1cd89ceb5bf69460613f3ee91c698"
+const VITE_COMMIT_VERSION = "eed1ef2d3594df240d2e2870f1fadd8af5818d65"
 const VITE_BUILD_VERSION = "v2.11.1"
