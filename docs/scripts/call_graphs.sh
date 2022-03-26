c3630a61e2816e71efe8a532b132f449c89e3739
niklr <r.niklaus@gmx.ch>
2022-03-26 12:22:59 -0700

    docs: fix call_graphs.sh script

--- docs/scripts/call_graphs.sh
+++ docs/scripts/call_graphs.sh
@@ -30,9 +30,7 @@ rm -rf $DIAGRAMS_DIR
 mkdir -p $DIAGRAMS_DIR
 
 $TOOL_PATH $BASE_DIR -start 'SendRawTransaction' -output $DIAGRAMS_DIR/send_raw_transaction.png -noShow -language 'go'
-# Current limitation: All methods with the same name will be included in the call graph diagram
-# Temporary workaround: Rename the update function in ledger/consensus/trigger.go to ConsensusTriggerUpdate
-# $TOOL_PATH $BASE_DIR -start 'ConsensusTriggerUpdate' -output $DIAGRAMS_DIR/consensus_trigger_update.png -noShow -language 'go'
+$TOOL_PATH $BASE_DIR -start $BASE_DIR/ledger/consensus/trigger.go:update -output $DIAGRAMS_DIR/consensus_trigger_update.png -noShow -language 'go'
 
 # Delete .dot files
 rm $DIAGRAMS_DIR/*.dot
