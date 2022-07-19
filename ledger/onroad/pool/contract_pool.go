73464329ba39f934b5b21672b527b1226fb87b0a
viteshan <wangjie@vite.org>
2022-07-19 22:12:20 +0800

    fix: update onroad db value

--- ledger/onroad/pool/contract_pool.go
+++ ledger/onroad/pool/contract_pool.go
@@ -410,7 +410,7 @@ func (cc *callerCache) lazyUpdateFrontTx(reader chainReader, hv *orHeightValue) 
 				if sub.FromHash == sendBlock.Hash {
 					j := uint32(i)
 					sub.FromIndex = &j
-					cc.storage.updateSubIndex(*sub)
+					cc.storage.updateFromIndex(*sub)
 				}
 			}
 		}
