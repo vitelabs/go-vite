af5c2c98d8e39ef0d55e3f27b722fb6670b9a1e7
LeonardoBill <LeonardoBill@users.noreply.github.com>
2023-02-19 09:56:03 +0000

    release v2.13.0

--- version/buildversion.go
+++ version/buildversion.go
@@ -1,3 +1,3 @@
 package version
-const VITE_COMMIT_VERSION = "068ccc4ba85bafa99d6a179a42380f3bca3f8172"
-const VITE_BUILD_VERSION = "v2.13.0-rc1"
+const VITE_COMMIT_VERSION = "c47fc7729d1a8f27cff4281bf2af6927c7e711bf"
+const VITE_BUILD_VERSION = "v2.13.0"
