86456f0354526a6e7ff139f89e811e1b303c1308
viteshan <viteshan@users.noreply.github.com>
2024-09-25 12:16:41 +0000

    release v2.14.0

--- version/buildversion.go
+++ version/buildversion.go
@@ -1,3 +1,3 @@
 package version
-const VITE_COMMIT_VERSION = "88b8637f09933e1ccf062a4e64160dec80a1b2ab"
-const VITE_BUILD_VERSION = "v2.14.0-rc1"
+const VITE_COMMIT_VERSION = "142a4f6fa0ed50d24ceaa6445e03da91b6bf8999"
+const VITE_BUILD_VERSION = "v2.14.0"
